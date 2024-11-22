79a0fbc248f9cce429843432fe8d97c86e8cc1c3
saowalak-tnk <67319010038@tnk.ac.th>
2024-11-22 16:17:42 +0700

    Update oop_student.py

--- oop_week6/oop_student.py
+++ oop_week6/oop_student.py
@@ -1,15 +1,22 @@
 import random 
 
 class Student :
-    def __init__(self,ชื่อนามสกุล,ชื่อเล่น,):
+    def __init__(self,ชื่อนามสกุล,ชื่อเล่น):
         self.name = ชื่อนามสกุล
         self.nickname = ชื่อเล่น
         self.score = random.randint(1,10)
+        self.editscore =random.randint(1,10)
     def Scores(self):
         if self.score >= 5:
             print(f"{self.name} {self.nickname} {self.score} : คุณสอบผ่าน")
         else :
             print(f"{self.name} {self.nickname} {self.score} : คุณสอบตก")
+            print("---------------------สอบแก้---------------------")
+            if self.editscore >= 5:
+                print(f"{self.name} {self.nickname} {self.editscore} : ผ่าน")
+            else :
+                print(f"{self.name} {self.nickname} {self.editscore} : ตก")
+
 student1 =Student("Chaiyanan yimsawat","Queue",)
 student2 =Student("Chutima Siritham","Sen",)
 student3 =Student("Gamolsak Chirangphan","Aof",)
