99500d0c16ffaff92e449f9e7f2a0282c73dada1
saowalak-tnk <67319010038@tnk.ac.th>
2025-01-28 14:47:40 +0700

    ใบงานที่7
    ......

--- oop_week8/studentoop.py
+++ oop_week8/studentoop.py
@@ -1,7 +1,52 @@
-class Student :
-    def __init__(self,name,id,age,grades):
-        self.Name = name
-        self.Id = id
-        self.Age = age
-        self.Grades = grades 
-    def sco
+class Student:
+    def __init__(self,ชื่อ,รหัสนักศึกษา,อายุ,เกรด):
+        self.name = ชื่อ
+        self.id = รหัสนักศึกษา
+        self.age = อายุ
+        self.grades = เกรด
+
+    def updateScore(self,คณิต,ไทย,อังกฤษ,วิทย์,สังคม):
+        self.grades["คณิต"] = คณิต
+        self.grades["ไทย"] = ไทย
+        self.grades["อังกฤษ"] = อังกฤษ
+        self.grades["วิทย์"] = วิทย์
+        self.grades["สังคม"] = สังคม
+
+    def scoreToGrade(self):
+        for subject, score in self.grades.items():
+            if score >= 80:
+                self.grades[subject] = 4
+            elif score >= 70:
+                self.grades[subject] = 3
+            elif score >= 60:
+                self.grades[subject] = 2
+            elif score >= 50:
+                self.grades[subject] = 1
+            else :
+                self.grades[subject] = 0
+
+    def calculateGrade(self):
+        return sum(self.grades.values()) / len(self.grades)
+    
+    def showInformation(self):
+        return f"ชื่อ : {self.name} หมายเลขประจำตัว : {self.id} อายุ : {self.age} เกรดเฉลี่ย : {self.calculateGrade()}"
+        
+        
+
+student1 = Student("ปิติพงศ์","67319010031",19, {})
+
+student1.updateScore(80,45,60,70,80)
+
+student1 = Student("เสาวลักษณ์","67319010038",19, {})
+
+student1.updateScore(100,70,80,50,100)
+
+
+print(student1.grades)
+
+student1.scoreToGrade()
+print(student1.grades)
+
+print(f"{student1.name} ได้เกรดเฉลี่ย {student1.calculateGrade()}")
+
+print(student1.showInformation())
